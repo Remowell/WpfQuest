340531badd9c15a7300e59ef9c90c770729d7df8
Remowell <pl712993@gmail.com>
2020-04-24 02:13:21 +0500

    Level 4 (Who is dungeon master?)

--- WpfQuest/MainWindow.xaml.cs
+++ WpfQuest/MainWindow.xaml.cs
@@ -26,9 +26,12 @@ namespace WpfQuest
         int time = 11;
         int counter = 0;
         private DispatcherTimer Timer;
+        public int endurance;
+        public int endurChar;
 
         public MainWindow()
         {
+            MessageBox.Show("You have only one attempt to answer at ANY level. Good luck");
             InitializeComponent();
             Timer = new DispatcherTimer();
             Timer.Interval = new TimeSpan(0, 0, 1);
@@ -46,7 +49,7 @@ namespace WpfQuest
             else
             {
                 Timer.Stop();
-                MessageBox.Show("Next time");
+                MessageBox.Show("Next time.");
                 Application.Current.Shutdown();
             }
         }
@@ -70,9 +73,9 @@ namespace WpfQuest
             {
                 Content = "Level 2",
                 FontSize = 20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Center
             };
-            labelLvl2.VerticalAlignment = VerticalAlignment.Top;
-            labelLvl2.HorizontalAlignment = HorizontalAlignment.Center;
             myGrid.Children.Add(labelLvl2);
 
             TextBox textBoxLvl2 = new TextBox
@@ -85,15 +88,17 @@ namespace WpfQuest
             Button buttonLvl2 = new Button
             {
                 Content = "Enter your answer",
-                FontSize = 20
+                FontSize = 20,
+                VerticalAlignment = VerticalAlignment.Bottom
             };
             buttonLvl2.Click += ButtonLvl2_Click;
-            buttonLvl2.VerticalAlignment = VerticalAlignment.Bottom;
             myGrid.Children.Add(buttonLvl2);
 
-            Label labelHelp = new Label();
-            labelHelp.Content = "You should go { level3 }";
-            labelHelp.VerticalAlignment = VerticalAlignment.Top;
+            Label labelHelp = new Label()
+            {
+                Content = "You should go { level3 }",
+                VerticalAlignment = VerticalAlignment.Top
+            };
             myGrid.Children.Add(labelHelp);
         }
 
@@ -109,7 +114,7 @@ namespace WpfQuest
                     }
                     else 
                     {
-                        MessageBox.Show("Bye");
+                        MessageBox.Show("Bye.");
                         Application.Current.Shutdown();
                     }
                 }
@@ -124,15 +129,17 @@ namespace WpfQuest
             Label labelLvl3 = new Label
             {
                 Content = "Level 3",
-                FontSize = 20
+                FontSize = 20,
+                VerticalAlignment = VerticalAlignment.Top,
+                HorizontalAlignment = HorizontalAlignment.Center
             };
-            labelLvl3.VerticalAlignment = VerticalAlignment.Top;
-            labelLvl3.HorizontalAlignment = HorizontalAlignment.Center;
             myGrid.Children.Add(labelLvl3);
 
-            Label labelHelp2 = new Label();
-            labelHelp2.Content = "Choose the color you see HERE";
-            labelHelp2.VerticalAlignment = VerticalAlignment.Top;
+            Label labelHelp2 = new Label()
+            {
+                Content = "Choose the color you see HERE",
+                VerticalAlignment = VerticalAlignment.Top
+            };
             myGrid.Children.Add(labelHelp2);
 
             ComboBox comboBoxLvl3 = new ComboBox();
@@ -154,10 +161,10 @@ namespace WpfQuest
             Button buttonLvl3 = new Button
             {
                 Content = "Enter your answer",
-                FontSize = 20
+                FontSize = 20,
+                VerticalAlignment = VerticalAlignment.Bottom
             };
             buttonLvl3.Click += ButtonLvl3_Click;
-            buttonLvl3.VerticalAlignment = VerticalAlignment.Bottom;
             myGrid.Children.Add(buttonLvl3);
         }
 
@@ -173,12 +180,228 @@ namespace WpfQuest
                     }
                     else 
                     { 
-                        MessageBox.Show("We don't see this color");
+                        MessageBox.Show("We don't see this color.");
                         Application.Current.Shutdown();
                     }
                 }
             }
-            EndGame();
+            CreateLevelFour();
+        }
+
+        private void CreateLevelFour()
+        {
+            myGrid.Children.Clear();
+
+            Label labelLvl4 = new Label
+            {
+                FontSize = 20,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Content = "Level 4"
+            };
+            myGrid.Children.Add(labelLvl4);
+
+            GroupBoxChar();
+            GroupBoxItems();
+
+            Button buttonDung = new Button
+            {
+                Content = "Dungeon is waiting",
+                FontSize = 20,
+                Margin = new Thickness(0, 200, 0, 0)
+            };
+            buttonDung.Click += ButtonDung_Click;
+            myGrid.Children.Add(buttonDung);
+        }
+
+        private void GroupBoxChar()
+        {
+            GroupBox groupBoxChar = new GroupBox
+            {
+                Margin = new Thickness(0, 50, 0, 0)
+            };
+            groupBoxChar.Header = "Character";
+            StackPanel stackPanelChar = new StackPanel();
+
+            RadioButton radioButtonWarrior = new RadioButton
+            {
+                Content = "Warrior",
+                FontSize = 15,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            stackPanelChar.Children.Add(radioButtonWarrior);
+
+            RadioButton radioButtonMag = new RadioButton
+            {
+                Content = "Mage",
+                FontSize = 15,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            stackPanelChar.Children.Add(radioButtonMag);
+
+            groupBoxChar.Content = stackPanelChar;
+            myGrid.Children.Add(groupBoxChar);
+        }
+
+        private void GroupBoxItems()
+        {
+            GroupBox groupBoxItems = new GroupBox
+            {
+                Margin = new Thickness(0, 110, 0, 0)
+            };
+            groupBoxItems.Header = "Items";
+
+            StackPanel stackPanelItems = new StackPanel();
+
+            CheckBox keyBox = new CheckBox
+            {
+                Content = "Old key",
+                FontSize = 15,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            stackPanelItems.Children.Add(keyBox);
+
+            CheckBox boneBox = new CheckBox
+            {
+                Content = "Dragon bones",
+                FontSize = 15,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            stackPanelItems.Children.Add(boneBox);
+
+            CheckBox mapBox = new CheckBox
+            {
+                Content = "Strange map",
+                FontSize = 15,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            stackPanelItems.Children.Add(mapBox);
+
+            CheckBox run = new CheckBox
+            {
+                Content = "Magic rune",
+                FontSize = 15,
+                HorizontalAlignment = HorizontalAlignment.Center
+            };
+            stackPanelItems.Children.Add(run);
+           
+            groupBoxItems.Content = stackPanelItems;
+            myGrid.Children.Add(groupBoxItems);
+        }
+
+        private void ButtonDung_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in myGrid.Children)
+            {
+                if (item is GroupBox box)
+                {
+                    FindStackPan(box);
+                }
+            }
+            if (endurChar > endurance) 
+            {
+                MessageBox.Show("You are very slow .. Therefore you are dead");
+                Application.Current.Shutdown(); 
+            }
+            else 
+            { 
+                MessageBox.Show("You defeated the ghost, found a way out of the dungeon and opened it. Next level");
+                EndGame();
+            }
+        }
+
+        private void FindStackPan(GroupBox groupBox)
+        {
+            if (groupBox.Content is StackPanel stackPanel)
+            {
+                WhatInStackPan(stackPanel);
+            }
+        }
+
+        private void WhatInStackPan(StackPanel stackPanel)
+        {
+            foreach (var element in stackPanel.Children)
+            {
+                if (element is RadioButton radioButton)
+                {
+                    MagOrWar(radioButton);
+                }
+                else if (element is CheckBox checkBox)
+                {
+                    WhatItem(checkBox);
+                }
+            }
+        }
+
+        private void MagOrWar(RadioButton radioButton)
+        {
+            if (radioButton.Content is "Mage")
+            {
+                if (radioButton.IsChecked is true)
+                {
+                    endurance = 50;
+                    MessageBox.Show("Powerful spells, but remember it's not a pack horse.");
+                }
+            }
+            else if (radioButton.Content is "Warrior")
+            {
+                if (radioButton.IsChecked is true)
+                {
+                    endurance = 75;
+                    MessageBox.Show("Strong body and battle ax. It is unlikely to be saved from magic ...");
+                }
+            }
+            else { MessageBox.Show("We need a hero"); }
+        }
+
+        private void WhatItem(CheckBox checkBox)
+        {
+            if (checkBox.Content is "Old key")
+            {
+                if (checkBox.IsChecked is true)
+                {
+                    endurChar += 25;
+                }
+                else
+                {
+                    MessageBox.Show("You didn't get a key. You couldn't get out of the dungeon. Now this is your tomb.");
+                    Application.Current.Shutdown();
+                }
+            }
+            else if (checkBox.Content is "Dragon bones")
+            {
+                if (checkBox.IsChecked is true)
+                {
+                    endurChar += 25;
+                }
+            }
+            else if (checkBox.Content is "Strange map")
+            {
+                if (checkBox.IsChecked is true)
+                {
+                    endurChar += 25;
+                }
+                else
+                {
+                    MessageBox.Show("Where is this damn way out?");
+                    Application.Current.Shutdown();
+                }
+            }
+            else
+            {
+                if (endurance == 75)
+                {
+                    if (checkBox.IsChecked is true) { endurChar += 25; }
+                    else 
+                    { 
+                        MessageBox.Show("An ancient ghost has smashed you to pieces ... A drop of magic wouldn't hurt");
+                        Application.Current.Shutdown();
+                    }
+                }
+                else
+                {
+                    if (checkBox.IsChecked is true) { endurChar += 25; }
+                }
+            }
         }
 
         private void EndGame()
@@ -192,10 +415,10 @@ namespace WpfQuest
             Label labelEnd = new Label
             {
                 Content = "Congratulations, you are the chosen one.",
-                FontSize = 30
+                FontSize = 30,
+                VerticalAlignment = VerticalAlignment.Center,
+                HorizontalAlignment = HorizontalAlignment.Center
             };
-            labelEnd.VerticalAlignment = VerticalAlignment.Center;
-            labelEnd.HorizontalAlignment = HorizontalAlignment.Center;
             myGrid.Children.Add(labelEnd);
         }
     }
